dbc8f09ae29224eb6d56b87ae0d5a0c6308b76bd
Tyler Allen <mynameistylerallen@gmail.com>
2017-07-07 18:03:08 -0700

    add select options to user input form and fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,11 @@ import { Beast } from './beast.model';
 export class AppComponent {
   masterBeastList: Beast [] = [
     new Beast("Andy", "grizzly bear", 2, "male", "Carnivore", "Cascadia", 3, "long naps", "onions"),
-    new Beast("Marcus", "white rhinoceros", 24, "female", "Herbivore", "African Trek", 3, "playing in dirt", "baths"),
     new Beast("Sandy", "penguin", 5, "female", "Carnivore", "Snow Zone", 3, "swimming", "vegetables")
   ];
 
   addBeast(newBeastFromChild: Beast) {
     this.masterBeastList.push(newBeastFromChild);
-    console.log("ping");
-    console.log(this.masterBeastList);
   }
 
 
