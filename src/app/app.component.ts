514dc58cdc0485da390925f2408eda6ad9127019
Tyler Allen <mynameistylerallen@gmail.com>
2017-07-10 00:44:45 -0700

    add styling to beast-form component and beast-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,10 @@ export class AppComponent {
   selectedBeast = null;
 
   masterBeastList: Beast [] = [
-    new Beast("Andy", "grizzly bear", 2, "male", "Carnivore", "Cascadia", 3, "long naps", "onions"),
-    new Beast("Sandy", "penguin", 5, "female", "Carnivore", "Snow Zone", 3, "swimming", "vegetables")
+    new Beast("Frank", "grizzly bear", 13, "male", "Carnivore", "Cascadia", 4, "long naps", "onions"),
+    new Beast("Sandy", "penguin", 1, "female", "Carnivore", "Snow Zone", 3, "swimming", "vegetables"),
+    new Beast("Clyde", "octopus", 1, "male", "Carnivore", "Water World", 2, "puppies", "sushi"),
+    new Beast("Gerald", "rhinoceros", 27, "male", "Herbivore", "African Trek", 6, "belly rubs", "extinction")
   ];
 
   addBeast(newBeastFromChild: Beast) {
